50b67bb113e8daff968173297430915a266fc143
miguelorl <miguel.orland@gmail.com>
2025-06-15 19:46:01 -0500

    Cambio flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -63,4 +63,7 @@ def index():
     return render_template('index.html', prediction=prediction_result, image_name=filename, probs=probabilities)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    if not os.path.exists(app.config ['UPLOAD_FOLDER']):
+        os.makedirs(app.config [ 'UPLOAD_FOLDER'])
+    app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT", default=5000))
+
